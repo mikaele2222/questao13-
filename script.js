5b3b87ebdc2855f1ea363eb26a6137a7fc161f38
mikaele2222 <mfs52@aluno.ifal.edu.br>
2024-11-28 17:57:24 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+ let numero = parseFloat(prompt("digite um numero:"));
+
+    if (numero % 2 === 0) {
+      
+        alert('voce digitou um numero par');
+    } else {
+
+        alert('voce digitou um numero impar');
+    }
